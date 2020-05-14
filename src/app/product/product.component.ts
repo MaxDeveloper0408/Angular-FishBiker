7d93db08345ef13346b576f356be6bbf817df054
Smart Max <maxdevsmart0408@gmail.com>
2020-05-15 02:24:40 +0900

    no message

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -3,7 +3,9 @@ import { ActivatedRoute } from '@angular/router';
 import { ProductService } from '../services/product.service';
 import * as _ from 'underscore';
 import { Product } from '../model/product';
-
+import { CartModalComponent } from '../modal/cartmodal.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ShoppingCart, CartProduct} from '../model/shoppingCart';
 
 @Component({
   templateUrl: './product.component.html',
@@ -11,8 +13,8 @@ import { Product } from '../model/product';
 export class ProductComponent implements OnInit {
   productId: number;
   productObj: any;
-
-  constructor(private activatedRoute: ActivatedRoute, private productService: ProductService) {
+  
+  constructor(private activatedRoute: ActivatedRoute, private productService: ProductService, private modalService: NgbModal) {
 
     this.productId = Number(this.activatedRoute.snapshot.queryParams.id);
 }
@@ -23,6 +25,12 @@ export class ProductComponent implements OnInit {
     }
   }
 
+  showCartModal(product: Product) {
+    const modalCharge = this.modalService.open(CartModalComponent);
+    modalCharge.componentInstance.product = product;
+    modalCharge.result.then(product => {
+    }, (reason) => {})
+  }
 
   getProductById(productId: number) {
     var products = this.productService.getProductFromLocalStorage();
